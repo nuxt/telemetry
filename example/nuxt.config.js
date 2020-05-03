880ff9a5c6e7d33fd284d98c76855668c1a0c8c2
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2020-05-03 22:27:18 +0300

    fix: updates from Pooya pair coding

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,5 +1,8 @@
 import _module from '..'
 
 export default {
-  modules: [_module]
+  modules: [_module],
+  '@nuxt/telemetry': {
+    url: 'https://6v4qwm51s4.execute-api.us-east-1.amazonaws.com/dev/save'
+  }
 }

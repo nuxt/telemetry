c900d0e45082298f05053ff91cea7e3a7072855e
pooya parsa <pooya@pi0.ir>
2020-05-11 01:20:07 +0200

    chore: use new sandbox endpoint

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -3,6 +3,6 @@ import _module from '..'
 export default {
   modules: [_module],
   telemetry: {
-    url: 'https://6v4qwm51s4.execute-api.us-east-1.amazonaws.com/dev/save'
+    // url: 'http://localhost:8888/send'
   }
 }

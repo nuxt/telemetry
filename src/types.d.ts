6e29d57bdedfc515c958feff0cb299abeb3df01a
Pooya Parsa <pyapar@gmail.com>
2020-11-22 01:26:26 +0100

    chore: update all dependencies and use siroc

--- src/types.d.ts
+++ src/types.d.ts
@@ -52,3 +52,15 @@ export interface GitData {
   owner: string
   name: string
 }
+
+declare module '@nuxt/types/config/index' {
+  interface NuxtOptions {
+    telemetry: boolean | Partial<{
+      debug: boolean
+      endpoint: string
+      seed: string
+      consent: 1
+      enabled: boolean
+    }>
+  }
+}

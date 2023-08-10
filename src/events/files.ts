aa8749fa6ce7cc9eeb4da08d70293df3caad76a9
Daniel Roe <daniel@roe.dev>
2023-08-10 14:49:42 +0100

    fix: resolve `app.config` path from srcDir (w/ exts)

--- src/events/files.ts
+++ src/events/files.ts
@@ -1,5 +1,6 @@
 import { resolve } from 'pathe'
 import { pathExists } from 'fs-extra'
+import { resolvePath } from '@nuxt/kit'
 import { EventFactory } from '../types'
 
 export interface FilesEvent {
@@ -14,7 +15,7 @@ export const files = <EventFactory<FilesEvent>> async function (context) {
 
   const nuxtIgnore = await pathExists(resolve(options.rootDir, '.nuxtignore'))
   const nuxtRc = await pathExists(resolve(options.rootDir, '.nuxtrc'))
-  const appConfig = await pathExists(resolve(options.rootDir, 'app.config.ts'))
+  const appConfig = await pathExists(await resolvePath('~/app.config'))
 
   return {
     name: 'files',

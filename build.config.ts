8565242e1bca59fbf344d4f43a5f8ed7f78880c4
Daniel Roe <daniel@roe.dev>
2025-02-11 12:14:47 +0000

    build: update to latest module-builder

--- build.config.ts
+++ build.config.ts
@@ -1,8 +1,23 @@
+import { readFile, writeFile } from 'node:fs/promises'
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   rollup: {
+    // remove in next major version
+    emitCJS: true,
     cjsBridge: false,
+  },
+  // remove in next major version
+  hooks: {
+    'build:done': async () => {
+      const declaration = await readFile('./dist/types.d.mts', 'utf-8')
+      if (!declaration.includes('export { type ModuleOptions, default } from \'./module.mjs\'')) {
+        process.exit(1)
+      }
+      await writeFile('./dist/types.d.cts', `
+import { default as telemetryModule } from './module.mjs'
+export = telemetryModule`)
+    },
   },
   entries: [
     'src/cli',

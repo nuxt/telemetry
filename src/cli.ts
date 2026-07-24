2b033c435de2abda63f53e28dbff10d979b4ee72
Computed Functions <215638926+computed-fn@users.noreply.github.com>
2026-07-24 20:08:05 +0700

    Fix CI for windows

--- src/cli.ts
+++ src/cli.ts
@@ -154,12 +154,15 @@ async function _checkDisabled(dir: string): Promise<string | false | undefined> 
     return 'by ' + resolve(dir, RC_FILENAME)
   }
 
+  // This is maintained for backward compatibility
   if (disabledByConf(rc.readUser({ name: RC_FILENAME }))) {
     return 'by ' + resolve(homedir(), RC_FILENAME)
   }
 
   if (disabledByConf(rc.readUserConfig({ name: RC_FILENAME }))) {
-    return 'by ' + resolve(homedir(), '.config', RC_FILENAME)
+    // Uses the same resolving logic as rc9
+    const rc9Base = process.env.XDG_CONFIG_HOME || resolve(homedir(), '.config')
+    return 'by ' + resolve(rc9Base, RC_FILENAME)
   }
 }
 

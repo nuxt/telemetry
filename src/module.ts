64964973ae50db10f365ad3dbdf9a490a6557b66
Daniel Roe <daniel@roe.dev>
2026-08-31 09:58:14 +0000

    fix: honour NUXT_TELEMETRY_DISABLED in the module itself

--- src/module.ts
+++ src/module.ts
@@ -5,7 +5,7 @@ import type { TelemetryOptions } from './types'
 import { ensureUserconsent } from './consent'
 import { logger } from './utils/log'
 import { randomSeed } from './utils/hash'
-import { isDoNotTrack } from './utils/env'
+import { isDoNotTrack, isTruthy } from './utils/env'
 import type { NuxtOptions } from 'nuxt/schema'
 
 export type ModuleOptions = TelemetryOptions
@@ -32,6 +32,11 @@ export default defineNuxtModule<TelemetryOptions>({
 
     if (isDoNotTrack()) {
       logger.info('Telemetry disabled by `DO_NOT_TRACK` environment variable')
+      return
+    }
+
+    if (isTruthy(process.env.NUXT_TELEMETRY_DISABLED)) {
+      logger.info('Telemetry disabled by `NUXT_TELEMETRY_DISABLED` environment variable')
       return
     }
 

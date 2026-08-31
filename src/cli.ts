d3fbfd8fb4321b99c55cf04863edab0f1b87c9d0
Daniel Roe <daniel@roe.dev>
2026-08-31 09:58:01 +0000

    fix: report the layer that actually disabled telemetry

--- src/cli.ts
+++ src/cli.ts
@@ -146,7 +146,7 @@ async function _checkDisabled(dir: string): Promise<string | false | undefined> 
     const config = await loadNuxtConfig({ cwd: dir })
     for (const layer of config._layers) {
       if (disabledByConf(layer.config)) {
-        return 'by ' + config._layers[0]!.configFile
+        return 'by ' + (layer.configFile ?? config._layers[0]!.configFile)
       }
     }
   }
